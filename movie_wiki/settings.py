8ea5dc36244a23396aa2ed685127d550829aa4b0
aptro <akashkupatro@gmail.com>
2016-02-21 18:18:38 +0530

    drf json renderer added

--- movie_wiki/settings.py
+++ movie_wiki/settings.py
@@ -109,6 +109,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 
 }
 
